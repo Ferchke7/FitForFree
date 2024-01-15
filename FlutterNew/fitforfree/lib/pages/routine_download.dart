82091456ca7853ddd058eba1db3220f84a9a8805
Ferchke17 <ferdavs@naver.com>
2024-01-15 19:53:08 +0900

    changed

--- FlutterNew/fitforfree/lib/pages/routine_download.dart
+++ FlutterNew/fitforfree/lib/pages/routine_download.dart
@@ -1,6 +1,7 @@
 import 'package:accordion/accordion.dart';
 import 'package:fitforfree/database/exercise_json_helper.dart';
 import 'package:fitforfree/database/sqlite_service.dart';
+import 'package:fitforfree/models/exercise.dart';
 import 'package:fitforfree/models/routine_dto.dart';
 import 'package:fitforfree/utils/common.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +15,20 @@ class RoutineDonwload extends StatefulWidget {
 }
 
 class _RoutineDonwloadState extends State<RoutineDonwload> {
+  UserService userService = UserService();
+  ExerciseService exerciseService = ExerciseService();
+  late List<Exercise> listExercise;
+  @override
+  void initState(){
+    super.initState();
+    
+  }
+
+  
+
+
   @override
   Widget build(BuildContext context) {
-    UserService userService = UserService();
-    ExerciseService exerciseService = ExerciseService();
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -36,12 +47,47 @@ class _RoutineDonwloadState extends State<RoutineDonwload> {
                           contentVerticalPadding: 20,
                           header: Text(
                             "Routine creator: ${widget.routineDTO.user}\ndescription:\n${widget.routineDTO.routineDescription}",
-                            style: TextStyle(color: Colors.white),
+                            style: const TextStyle(color: Colors.white),
                           ),
                           content: SingleChildScrollView(
                               child: Column(
                             children: <Widget>[
-                              Text("Routine for monday: ${widget.routineDTO.monday}")
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                      "Routine for monday: \n${widget.routineDTO.monday}"),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                      "Routine for tuesday: \n${widget.routineDTO.tuesday}"),
+                              ),
+                                   Padding(
+                                     padding: const EdgeInsets.all(8.0),
+                                     child: Text(
+                                      "Routine for wednesday: \n${widget.routineDTO.wednesday}"),
+                                   ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                      "Routine for thursday: \n${widget.routineDTO.thursday}"),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                      "Routine for friday: \n${widget.routineDTO.friday}"),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                      "Routine for saturday: \n${widget.routineDTO.saturday}"),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                      "Routine for sunday: \n${widget.routineDTO.sunday}"),
+                                    ),
+                                     
                             ],
                           )))
                     ])
@@ -63,17 +109,18 @@ class _RoutineDonwloadState extends State<RoutineDonwload> {
                         userTemp?.saturday = widget.routineDTO.saturday;
                         setState(() {
                           userService.updateUser(userTemp!);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                        duration: Duration(seconds: 1),
-                        content: Text('Updated'),
-                        backgroundColor: Color.fromARGB(255, 4, 213, 77),
-                      ),);
-                        Navigator.pop(context);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              duration: Duration(seconds: 1),
+                              content: Text('Updated'),
+                              backgroundColor: Color.fromARGB(255, 4, 213, 77),
+                            ),
+                          );
+                          Navigator.pop(context);
                         });
                       },
-                      icon: Icon(Icons.download),
-                      label: Text("Download"))
+                      icon: const Icon(Icons.download),
+                      label: const Text("Download and use this routine"))
                 ],
               )
             ],
--- FlutterNew/fitforfree/lib/pages/share_routine.dart
+++ FlutterNew/fitforfree/lib/pages/share_routine.dart
@@ -27,35 +27,51 @@ class _ShareRoutinePageState extends State<ShareRoutinePage> {
     routinesBack = fetchRoutines();
   }
 
-  @override dispose() {
+  @override
+  dispose() {
     super.dispose();
     routineName.dispose();
     routineDescription.dispose();
   }
 
-  // Future<void> deleteCard(int postId) {
-    
-  // }
-  Future<void> postData(RoutineDTO routineDTO) async {
-  final accessToken = client.auth.currentSession?.accessToken;
-  const String apiUrl =
-      'http://192.227.152.231:3333/Exercise';
-  final response = await http.post(
-    Uri.parse(apiUrl),
-    headers: {
-      HttpHeaders.contentTypeHeader: "application/json",
-      HttpHeaders.authorizationHeader: "Bearer $accessToken"
-    },
-    body: jsonEncode(routineDTO.toJson()),
-  );
-
-  if (response.statusCode == 200) {
-    debugPrint('Post Successful!');
-  } else {
-    debugPrint('Failed to post data. Error: ${response.statusCode}');
+  Future<void> deleteMyRoutine(String routineName) async {
+    try {
+      final accessToken = client.auth.currentSession?.accessToken;
+      final response = await http.delete(
+        Uri.parse("http://192.227.152.231:3333/Exercise?routineName=$routineName"),
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+          HttpHeaders.authorizationHeader: "Bearer $accessToken"
+        },
+      );
+      if (response.statusCode == 200) {
+        debugPrint("Deleted post with ${routineName}");
+      } else {
+        debugPrint("unsuccessfull${response.statusCode}");
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
-}
 
+  Future<void> postData(RoutineDTO routineDTO) async {
+    final accessToken = client.auth.currentSession?.accessToken;
+    const String apiUrl = 'http://192.227.152.231:3333/Exercise';
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+        HttpHeaders.authorizationHeader: "Bearer $accessToken"
+      },
+      body: jsonEncode(routineDTO.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint('Post Successful!');
+    } else {
+      debugPrint('Failed to post data. Error: ${response.statusCode}');
+    }
+  }
 
   Future<List<RoutineDTO>> fetchRoutines() async {
     try {
@@ -87,174 +103,191 @@ class _ShareRoutinePageState extends State<ShareRoutinePage> {
       throw Exception('Failed to load posts');
     }
   }
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    
-    body: Column(
-      children: <Widget>[
-        Expanded( 
-          child: FutureBuilder(
-            future: routinesBack,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Container();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(
-                  child: Text(
-                    'No routines available.')
-                );
-              } else {
-                List<RoutineDTO> routinesTemp = snapshot.data!;
-                return ListView.builder(
-                  itemCount: routinesTemp.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
 
-                            ListTile(
-                              title: Text(routinesTemp[index].routineName, 
-                              style: const TextStyle(color: Colors.indigo,)),
-                              subtitle: Text(
-                                'Created by: ${routinesTemp[index].user}\n\n${routinesTemp[index].routineDescription}...',
-                                style: const TextStyle(color: Colors.indigo),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: FutureBuilder(
+                future: routinesBack,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No routines available.'));
+                  } else {
+                    List<RoutineDTO> routinesTemp = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: routinesTemp.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
                               children: <Widget>[
-                                const SizedBox(
-                                  width: 3,
-                                  height: 10,
+                                ListTile(
+                                  title: Text(routinesTemp[index].routineName,
+                                      style: const TextStyle(
+                                        color: Colors.indigo,
+                                      )),
+                                  subtitle: Text(
+                                    'Created by: ${routinesTemp[index].user}\n\n${routinesTemp[index].routineDescription}...',
+                                    style:
+                                        const TextStyle(color: Colors.indigo),
+                                  ),
                                 ),
-                                routinesTemp[index].user == my_username ?
-                                IconButton(onPressed: () {},
-                                 icon: const Icon(Icons.delete), color: Colors.indigo,)
-                                : Container(),
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo),
-                                  child: const Text("donwload", style: TextStyle(color: Colors.white),),
-                                  onPressed: () {
-                                    Navigator.push(context,
-                                     MaterialPageRoute(builder: (context) => RoutineDonwload(routineDTO: routinesTemp[index],)
-                                     ));
-                                  },
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    const SizedBox(
+                                      width: 3,
+                                      height: 10,
+                                    ),
+                                    routinesTemp[index].user == my_username
+                                        ? IconButton(
+                                            onPressed: () {
+                                              //TODO ADD HERE DELETE FUNC
+                                              deleteMyRoutine(routinesTemp[index].routineName);
+                                              setState(() {
+                                                routinesBack = fetchRoutines();
+                                              });
+                                            },
+                                            icon: const Icon(Icons.delete),
+                                            color: Colors.indigo,
+                                          )
+                                        : Container(),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.indigo),
+                                      child: const Text(
+                                        "donwload",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RoutineDonwload(
+                                                      routineDTO:
+                                                          routinesTemp[index],
+                                                    )));
+                                      },
+                                    ),
+                                    const SizedBox(
+                                      width: 3,
+                                      height: 10,
+                                    ),
+                                  ],
                                 ),
-                                const SizedBox(
-                                  width: 3,
-                                  height: 10,
-                                ),
-                                
                               ],
                             ),
-                          ],
-                        ),
-                      ),
+                          ),
+                        );
+                      },
                     );
-                  },
-                );
-              }
-            },
-          ),
-        ),
-      ],
-    ),
-    floatingActionButton: FloatingActionButton.small(
-      backgroundColor: Colors.indigo,
-      onPressed: () => showDialog(context: context, 
-      builder: (BuildContext context) => Dialog.fullscreen(
-        child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const Text('Share your routine\nYour current routine will be shared'),
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: const Text("Name of Routine"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: 250,
-                child: TextFormField(
-                controller: routineName,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  labelText: "Name of Routine"
-                ),
-              ),
+                  }
+                },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: const Text("Description of routine"),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              controller: routineDescription,
-              autofocus: true,
-              
-              decoration: const InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 100),
-                border: OutlineInputBorder(),
-                labelText: "Description"
-              ),
-            ),
-            ),
-            const Divider(),
-            ElevatedButton(onPressed: () async {
-              User? user = await userService.getUserByUsername(my_username!);
-              
-              debugPrint("GET USER SERVICE:");
-              debugPrint(user?.monday.toString());
-              RoutineDTO routineDTO = RoutineDTO
-              (routineName: routineName.text, routineDescription: routineDescription.text,
-               monday: user?.monday, 
-               tuesday: user?.tuesday, 
-               wednesday: user?.wednesday, 
-               thursday: user?.thursday, 
-               friday: user?.friday, 
-               saturday: user?.saturday, 
-               sunday: user?.sunday,
-                user: my_username!);
-              
-              debugPrint(routineDTO.tuesday.toString());
-              routineDescription.clear();
-              routineName.clear();
-              await postData(routineDTO);
-              
-              setState(() {
-                routinesBack = fetchRoutines();
-                Navigator.pop(context);
-                
-              });
-              
-            }, child: const Text("Share ")),
-            ElevatedButton(onPressed: () {
-              Navigator.pop(context);
-            }, child: const Text("Cancel"))
           ],
         ),
-      ))
-    ),
-      child: const Icon(Icons.add, color: Colors.white),
-  ));
-}
+        floatingActionButton: FloatingActionButton.small(
+          backgroundColor: Colors.indigo,
+          onPressed: () => showDialog(
+              context: context,
+              builder: (BuildContext context) => Dialog.fullscreen(
+                      child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Text(
+                              'Share your routine\nYour current routine will be shared'),
+                        ),
+                        const Divider(),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: const Text("Name of Routine"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 250,
+                            child: TextFormField(
+                              controller: routineName,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                      EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                  labelText: "Name of Routine"),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: const Text("Description of routine"),
+                        ),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            controller: routineDescription,
+                            autofocus: true,
+                            decoration: const InputDecoration(
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(10, 10, 10, 100),
+                                border: OutlineInputBorder(),
+                                labelText: "Description"),
+                          ),
+                        ),
+                        const Divider(),
+                        ElevatedButton(
+                            onPressed: () async {
+                              User? user = await userService
+                                  .getUserByUsername(my_username!);
 
+                              debugPrint("GET USER SERVICE:");
+                              debugPrint(user?.monday.toString());
+                              RoutineDTO routineDTO = RoutineDTO(
+                                  routineName: routineName.text,
+                                  routineDescription: routineDescription.text,
+                                  monday: user?.monday,
+                                  tuesday: user?.tuesday,
+                                  wednesday: user?.wednesday,
+                                  thursday: user?.thursday,
+                                  friday: user?.friday,
+                                  saturday: user?.saturday,
+                                  sunday: user?.sunday,
+                                  user: my_username!);
+
+                              debugPrint(routineDTO.tuesday.toString());
+                              routineDescription.clear();
+                              routineName.clear();
+                              await postData(routineDTO);
+
+                              setState(() {
+                                routinesBack = fetchRoutines();
+                                Navigator.pop(context);
+                              });
+                            },
+                            child: const Text("Share ")),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("Cancel"))
+                      ],
+                    ),
+                  ))),
+          child: const Icon(Icons.add, color: Colors.white),
+        ));
+  }
 }
